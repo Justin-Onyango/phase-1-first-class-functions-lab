b329cc4542ae3140145d52f7f7de36e0c7f5da1c
Justin Onyango <ojustin446@gmail.com>
2024-03-20 22:14:05 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function () {
+    return ['Antonia', 'Nuru'];
+}
+const returnLastTwoDrivers= function () {
+    return ['Amari', 'Mo'];
+}
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier=function () {
+    return function (fareMultiplier) {
+        return (fareMultiplier)*(fareMultiplier);
+    } 
+}
+
+const fareDoubler= function fareMultiplier() {
+    return fareMultiplier^20;
+}
+
+const fareTripler= function fareMultiplier() {
+    return fareMultiplier^36;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+        
+}
